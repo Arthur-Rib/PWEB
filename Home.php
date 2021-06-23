1abcea3a043919868a024c0b2a602068805aa3c4
Arthur-Rib <86356093+Arthur-Rib@users.noreply.github.com>
2021-06-23 08:47:44 -0300

    Update Home.php

--- Home.php
+++ Home.php
@@ -9,13 +9,13 @@ session_start();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo $_SESSION['Nome'] ?></title>
+    <title><?php echo $_SESSION['name'] ?></title>
 </head>
 
 <body>
     <div class="container">
-        Nome: <?php echo $_SESSION['Nome'] ?>
-        Telefone: <?php echo $_SESSION['Telefone'] ?>
+        Nome: <?php echo $_SESSION['name'] ?>
+        Telefone: <?php echo $_SESSION['phone'] ?>
     </div>
 </body>
 
